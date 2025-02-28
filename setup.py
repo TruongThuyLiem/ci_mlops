3ef2cc7e4f6d81b2c0fa778f6e5358f16f4083f4
TruongThuyLiem <truongthithuyliem@gmail.com>
2025-02-28 10:05:19 +0700

    Initial cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='lesson about CI in mlops',
+    author='liemtruong',
+    license='',
+)
